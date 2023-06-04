733cb8a158ef9f228fb7d8d50300974ba4c65485
Martin Steiger <git@martin-steiger.de>
2023-06-04 10:36:43 +0200

    fix: test

---
+++ src/test/java/io/github/msteiger/ModbusTest.java
@@ -0,0 +1,18 @@
+package io.github.msteiger;
+
+import java.io.IOException;
+
+import org.junit.jupiter.api.Test;
+
+import io.github.msteiger.simpleModbus.Modbus;
+
+public class ModbusTest {
+
+    @Test
+    public void testSetup() throws IOException {
+//        try (Modbus mb = new Modbus("localhost")) {
+//            mb.setUnitIdentifier(3);
+//        }
+
+    }
+}
